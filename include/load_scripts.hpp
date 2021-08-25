41233e1a9ca80ad56ede018fae238b7d0696fe67
Christopher Rose <christophervrose@gmail.com>
2021-08-25 14:28:01 +0530

    [add] loading scripts

--- include/load_scripts.hpp
+++ include/load_scripts.hpp
@@ -1,3 +1,14 @@
 #include <string>
 
-void load(std::string);
+/**
+ * Compile a given shader
+*/
+uint compileShader(uint type, std::string& src);
+/**
+ * Compile a shader program from a given vertex and fragment source
+*/
+uint createShaderProgram(std::string& vsstr,std::string& fsstr);
+/**
+ * Create shader program from compiled shaders
+*/
+uint createShaderProgram(uint vs,uint fs);
--- src/load_scripts.cpp
+++ src/load_scripts.cpp
@@ -1,12 +1,57 @@
-#include<glad/glad.h>
-// #include<glfw
-#include<iostream>
-#include<load_scripts.hpp>
+#include <glad/glad.h>
+#include <iostream>
+#include <load_scripts.hpp>
 
-void load(std::string x){
-    std::cout<<x<<std::endl;
+uint compileShader(uint type, std::string& src)
+{
+    uint shader = glCreateShader(type);
+    const char* srcstr = src.c_str();
+    glShaderSource(shader, 1, &srcstr, nullptr);
+
+    glCompileShader(shader);
+
+    int result;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        // please, getting compilation stats is usually a pain
+        std::cout << "Compilation failed" << (type == GL_VERTEX_SHADER ? "VERTEX" : "FRAGMENT") << std::endl;
+        return 0;
+    }
+
+    return shader;
 }
 
-bool init_load(){
+uint createShaderProgram(std::string& vsstr, std::string& fsstr)
+{
+    uint vs = compileShader(GL_VERTEX_SHADER, vsstr);
+    uint fs = compileShader(GL_FRAGMENT_SHADER, fsstr);
+
+    if (vs == 0 || fs == 0) {
+        std::cout << "Program compilation requires vs+fs to compile" << std::endl;
+        return 0;
+    }
+    uint program = createShaderProgram(vs, fs);
+
+    if(program==0) return program;
+    //delete the intermediate shaders
+
     
+    glDeleteShader(fs);
+    glDeleteShader(vs);
+    return program;
+}
+
+uint createShaderProgram(uint vs, uint fs)
+{
+    if (vs == 0 || fs == 0) {
+        std::cout << "Program compilation requires vs+fs to compile" << std::endl;
+        return 0;
+    }
+    uint program = glCreateProgram();
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+
+    glLinkProgram(program);
+    glValidateProgram(program);
 }
