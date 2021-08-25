00b58c53cfa02d30b9cc2cc8bde89b3fee8c897c
Christopher Rose <christophervrose@gmail.com>
2021-08-25 14:43:30 +0530

    basic shaders

--- src/main.cpp
+++ src/main.cpp
@@ -14,29 +14,45 @@ int main(void)
     }
 
     // Any init stuff goes here
-    // float vertices[] = {
-    //     -0.5f,
-    //     -0.5f,
-    //     0.0f,
-    //     0.5f,
-    //     -0.5f,
-    //     0.0f,
-    //     0.0f,
-    //     0.5f,
-    //     0.0f
-    // };
-    // uint vbo;
-    // glGenBuffers(1,&vbo);
-    // glBindBuffer(GL_ARRAY_BUFFER,vbo);
+    float vertices[] = {
+        -0.0f,
+        -0.5f,
+
+        0.5f,
+        -0.5f,
+
+        0.0f,
+        0.5f,
+    };
+
+    uint vbo,vao;
+    glGenBuffers(1, &vbo);
+    glGenVertexArrays(1,&vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof vertices, &vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    glEnableVertexAttribArray(0);
+
+    std::string fb =
+#include <shaders/simple.frag>
+        ;
+
+    std::string vb =
+#include <shaders/simple.vert>
+        ;
+
+    uint shaderProgram = createShaderProgram(vb,fb);
+
+    glUseProgram(shaderProgram);
 
 
     // consider this as the game loop
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window.window()))
-    {
+    while (!glfwWindowShouldClose(window.window())) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         /* Swap front and back buffers */
         glfwSwapBuffers(window.window());
 
@@ -44,5 +60,6 @@ int main(void)
         glfwPollEvents();
     }
 
+    glDeleteProgram(shaderProgram);
     return 0;
 }
