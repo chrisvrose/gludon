8c79873a00ebc7fe82b77e9e290065bd2bbaf7ca
Atreya Bain <christophervrose@gmail.com>
2021-09-02 18:27:06 +0530

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -49,12 +49,14 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof vertices, &vertices, GL_STATIC_DRAW);
 
+    // ebo saves memory by representing an array of pointers to orig data
     uint ebo;
     glGenBuffers(1, &ebo);
     // binding with bound arraybuffer
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(eboindices), eboindices, GL_STATIC_DRAW);
-
+    
+    // TODO figure out what this acts on -> ebo or vbo
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
     glEnableVertexAttribArray(0);
 
