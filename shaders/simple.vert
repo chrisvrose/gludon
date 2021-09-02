8c55f4edf8cb1b900a186139c62dfcc0c8b94254
Atreya Bain <christophervrose@gmail.com>
2021-09-02 13:53:45 +0530

    [add] stb dependency

--- shaders/simple.vert
+++ shaders/simple.vert
@@ -2,10 +2,10 @@
 
 layout (location=0) in vec4 posit;
 layout (location=1) in vec4 colorin;
-
+uniform vec4 posoff;
 out vec4 colora;
 
 void main(){
-    gl_Position = posit;
+    gl_Position = posit+posoff;
     colora=colorin;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include <GLFW/glfw3.h>
 #include <glWindow.hpp>
 #include <load_scripts.hpp>
+#include<stb_image.h>
+// comment this to disable wireframe mode
+// #define GL_USE_WIREFRAME
+
 int main(void)
 {
     GLWindow window;
@@ -15,31 +19,41 @@ int main(void)
 
     // Any init stuff goes here
     float vertices[] = {
-        -0.0f,
-        -0.5f,
 
-        0.5f,0.0f,0.0f,
+        0.f,0.f,
 
-        0.5f,
-        -0.5f,
+        0.f,0.5f,0.f,
 
-        0.0f,0.5f,0.0f,
+        0.5f, 0.f,
 
-        0.0f,
-        0.5f,
+        0.5f,0.5f,0.0f,
+
+        0.0f, 0.5f,
 
         0.0f,0.0f,0.5f,
 
+        0.5f, .5f,
+
+        .5f,.0f,.0f
     };
+
+    uint eboindices[]={0,1,2,2,3,1};
 
     uint vbo,
         // remembers all the attribpointers and everything
-        vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+        vao[2];
+    glGenVertexArrays(2, vao);
+    glBindVertexArray(vao[0]);
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof vertices, &vertices, GL_STATIC_DRAW);
+
+    uint ebo;
+    glGenBuffers(1,&ebo);
+    // binding with bound arraybuffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(eboindices),eboindices,GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
     glEnableVertexAttribArray(0);
 
@@ -57,18 +71,34 @@ int main(void)
     uint shaderProgram = createShaderProgram(vb, fb);
     // std::cout << shaderProgram << std::endl;
     glUseProgram(shaderProgram);
-
+    float a[4] = {0};
     // consider this as the game loop
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window.window())) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(vao[0]);
+
+
+        uint offsetloc = glGetUniformLocation(shaderProgram,"posoff");
+        std::cout<<offsetloc<<std::endl;
+        glUniform4fv(offsetloc,1,a);
+        a[0]+=0.001;if(a[0]>1) a[0]=-1;
+        a[1]+=0.001;if(a[1]>1)a[1]=-1;
+
+
+        // glDrawArrays( GL_TRIANGLES, 0, sizeof(vertices)/sizeof(float));
+
+        #ifdef GL_USE_WIREFRAME
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        #endif
+        glDrawElements(GL_TRIANGLES,sizeof(eboindices)/sizeof(float),GL_UNSIGNED_INT,0);
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window.window());
 
+        
         /* Poll for and process events */
         glfwPollEvents();
     }
